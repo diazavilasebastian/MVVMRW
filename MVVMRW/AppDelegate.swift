41690f11c782606f84902ac2e8c489239e487d5a
sebastian <sebastiaan.diaz.avila@accenture.com>
2020-05-17 15:37:55 -0400

    Remove storyboard from init

--- MVVMRW/AppDelegate.swift
+++ MVVMRW/AppDelegate.swift
@@ -11,10 +11,12 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let navigation = UINavigationController(rootViewController: UIViewController(nibName: nil, bundle: nil))
+        window.rootViewController = navigation
+        window.makeKeyAndVisible()
         return true
     }
 
