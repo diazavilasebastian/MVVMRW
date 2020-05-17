32bd472a88f02fdf175ed81592d5fcd667c23522
sebastian <sebastiaan.diaz.avila@accenture.com>
2020-05-17 15:26:52 -0400

    Initial Commit

---
+++ MVVMRW/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMRW
+//
+//  Created by ITAU on 17-05-20.
+//  Copyright © 2020 iOSDemos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
